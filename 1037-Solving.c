350d78a5afe7f6d84906e6f7b4660457c5c01843
Minar-Hossain <minarh355@gmail.com>
2025-10-20 10:47:55 +0600

    new file added

---
+++ 1037-Solving.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+        double f;
+        scanf("%lf", &f);
+     if (f >= 0 && f <= 25.0000){
+       printf("Intervalo [0,25]\n");
+    } else if (f > 25.0000 && f <= 50.0000){
+       printf("Intervalo [25,50]\n");
+    } else if (f > 50.0000  && f <= 75.0000) {
+       printf("Intervalo [50,75]\n");
+    } else if(f > 75.0000 && f <= 100.0000 ){
+       printf("Intervalo [50,75]\n");
+    } else {
+       printf("Fora de inervalo\n");
+    }
+return 0;
+}
